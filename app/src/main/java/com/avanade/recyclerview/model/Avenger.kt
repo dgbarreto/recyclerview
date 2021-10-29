ad54b75d1ddba19e2cbc86235d955de36f8119dd
Danilo Barreto <danilo.g.barreto@avanade.com>
2021-10-29 15:03:03 -0300

    Starter code

--- app/src/main/java/com/avanade/recyclerview/model/Avenger.kt
+++
@@ -1,8 +0,0 @@
-package com.avanade.recyclerview.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class Avenger (
-    @StringRes val stringResourceId : Int,
-    @DrawableRes val imageResourceId : Int)
